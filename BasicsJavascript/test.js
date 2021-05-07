bb4969a0c315a2367c889d11b118ae53bf610c55
César González <you@example.com>
2021-05-07 08:51:52 -0700

    formateo pc

--- BasicsJavascript/test.js
+++ BasicsJavascript/test.js
@@ -1,8 +1,2 @@
-var age = 5;
-
-while (age < 10) {
-  console.log("Your age is less than 10");
-  age++;
-}
-
-document.write("you are now over 10");
+var myString = 'I\'m a "fun" string';
+console.log(myString.indexOf("string"));
